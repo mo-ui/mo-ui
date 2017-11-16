ad23d5c1dd52dd11db65fea7da40a9b0d5e4c14b
answershuto <905307789@qq.com>
2017-11-16 20:35:55 +0800

    支持MarkDown或者md后缀文件的example

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -72,6 +72,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.MarkDown$|\.md$/,
+        loader: 'vue-markdown-loader'
       }
     ]
   }
