5a8baa0be35a426c857a77e0695ba5ff4808fc6b
陈子云 <472206618@qq.com>
2017-11-18 19:46:10 +0800

    chore: set indent_size to 4 space

--- test/unit/specs/first.spec.js
+++ test/unit/specs/first.spec.js
@@ -1,26 +1,26 @@
 import {
-  createVue,
-  destroyVM
+    createVue,
+    destroyVM
 } from '../utils';
 
 describe('first test', () => {
-  let vm = createVue({
-    template: `
-        <p>{{ text }}</p>
-    `,
-    data() {
-      return {
-        text: 'hello world'
-      };
-    }
-  }, true);
+    let vm = createVue({
+        template: `
+            <p>{{ text }}</p>
+        `,
+        data() {
+            return {
+                text: 'hello world'
+            };
+        }
+    }, true);
 
-  it('should render correct contents', () => {
-    expect('hello world').toEqual('hello world')
-  })
+    it('should render correct contents', () => {
+        expect('hello world').toEqual('hello world')
+    })
 
-  afterEach(() => {
-    destroyVM(vm);
-  });
+    afterEach(() => {
+        destroyVM(vm);
+    });
 })
 
--- test/unit/utils.js
+++ test/unit/utils.js
@@ -3,13 +3,13 @@ import Vue from 'vue';
 let id = 0;
 
 const createElm = function () {
-  const elm = document.createElement('div');
+    const elm = document.createElement('div');
 
-  id += 1;
-  elm.id = `app${id}`;
-  document.body.appendChild(elm);
+    id += 1;
+    elm.id = `app${id}`;
+    document.body.appendChild(elm);
 
-  return elm;
+    return elm;
 };
 
 /**
@@ -17,7 +17,7 @@ const createElm = function () {
  * @param  {Object} vm
  */
 exports.destroyVM = function (vm) {
-  vm.$el && vm.$el.parentNode && vm.$el.parentNode.removeChild(vm.$el);
+    vm.$el && vm.$el.parentNode && vm.$el.parentNode.removeChild(vm.$el);
 };
 
 /**
@@ -27,11 +27,11 @@ exports.destroyVM = function (vm) {
  * @return {Object} vm
  */
 exports.createVue = function (Compo, mounted = false) {
-  if (Object.prototype.toString.call(Compo) === '[object String]') {
-    Compo = {
-      template: Compo
-    };
-  }
-  return new Vue(Compo).$mount(mounted === false ? null : createElm());
+    if (Object.prototype.toString.call(Compo) === '[object String]') {
+        Compo = {
+            template: Compo
+        };
+    }
+    return new Vue(Compo).$mount(mounted === false ? null : createElm());
 };
 
