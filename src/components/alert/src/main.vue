0a8b326a70d3772868c53b06fc33b8b4b7d44622
catchme <izengdp@163.com>
2018-05-02 17:59:48 +0800

    完善 button

---
+++ src/components/alert/src/main.vue
@@ -0,0 +1,22 @@
+<template>
+    <transition name="mo-alert-fade">
+        <div
+            class="mo-alert"
+            :class="[typeClass, center ? 'is-center' : '']"
+             v-show="visible"
+             role="alert"
+             >
+
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: "main"
+    }
+</script>
+
+<style scoped>
+
+</style>
